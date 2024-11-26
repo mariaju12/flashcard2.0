96addb60c99ee07aaca930ae5a38543b7d80c1ed
mariaju12 <00001121349808sp@al.educacao.sp.gov.br>
2024-11-26 13:55:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'TikTok',
+    'Quem mora na vinhosa?',
+    'A Rayanne'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são as capitais da Africa do Sul?',
+    'Cidade do Cabo, Pretoria, Bloemfotein'
+)
+
+criaCartao(
+    'Geral',
+    'Primeira mulher a ganhar um prêmio nobel?',
+    'Marie Curie'
+)
+
+criaCartao(
+    'TikTok',
+    'Quem casou na vinhosa',
+    'Veraldo e Rayane'
+)
